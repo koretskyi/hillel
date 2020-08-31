a76ddd7502a0fea64eec2607b4b26ad07b920856
koretskyi <koretskyi@gmail.com>
2020-08-31 15:20:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//1. Написать программу расчета "второго закона Ньютона"
+
+// let f = prompt('Enter F value');
+// let m = prompt('Enter m value');
+// let a = f * m;
+
+// document.write(`<p> ************** </br> Ускорение тела при силе F = ${f}, и массе m = ${m} : </br> -------------------- 
+// </br> a = ${a} </br> -------------------- </br> end. </p>`);
+
+
+//2. Написать программу которая получит от 3ьох пользователя информацию возраст, имя, фамилию. Ввести информацию в таком виде:
+
+// let firstStudent = prompt('enter age, first and last name');
+// let secondStudent = prompt('enter age, first and last name');
+// let thirdStudent = prompt('enter age, first and last name');
+
+// console.log(` ************** \n Список студентов зарегестрировавшихся на вебинар: \n 1. ${firstStudent} \n 2. ${secondStudent} \n 3. ${thirdStudent} \n -------------- \n Средний возраст студента: ${(parseInt(firstStudent) + parseInt(secondStudent) + parseInt(thirdStudent)) / 3} \n -------------- \n **************`);
+
+// //3. Реализовать cкрипт универсльного калькулятора который получает от пользователя два значения (a, b), и выводит в консоль результат всех арифметических операций (+, -, *, /).
+// let a = +prompt('enter a value');
+// let b = +prompt('enter b value');
+
+// console.log(` ************** \n При а = ${a}, b = ${b}\n ************** \n Результат: \n -------------------- \n
+//  a + b = ${ a + b }; a - b = ${ a - b }; a * b = ${ a * b }; a / b = ${ a / b }; \n ------------------- \n **************`);
