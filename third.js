46de987b9f657893bef8197addecab002d03ba2a
koretskyi <koretskyi@gmail.com>
2020-09-20 02:04:01 +0300

    Add files via upload

--- third.js
+++ third.js
@@ -1,20 +1,38 @@
-// В одномерном массиве произвести такую замену:
-// 1 элемент поменять с 2
-// 3 элемент поменять с 4
-// // 5 элемент поменять с 6 и тд Если массив непарный - последний элемент не трогать.
+1. Заполнить массив А случайными числами (диазпазон на ваше усмотрение). Заменить каждый элемент массива с нечетным индексом на ноль.
 
-// let arr = [1, 2, 3, 4, 5];
+let arr = new Array(40);
 
-// let arrRes = new Array(arr.length);
+for ( i = 0; i < arr.length; i++ ) {
+    arr[i] = (Math.round( Math.random() * 50 ));
+    if(i % 2 !== 0 ) {
+        arr[i] = 0;
+    }
+}
 
-// for ( i = 1; i <= arr.length; i = i + 2 ) {
-//     if(arr[i] === undefined) {
-//     	arrRes[i - 1] = arr[i - 1];
-//     	break;
-//     }
+2. Заполнить массив размерностью n значениями с клавиатуры. Если введенное значение пользователя не число, или отрицательное значение, то запросить повторный ввод данных.
 
-//     arrRes[i - 1] = arr[i];
-//     arrRes[i] = arr[i - 1];
-// }
+let arr = new Array(5);
 
-// console.log(arrRes);
+for ( i = 0; i < arr.length; i++ ) {
+    let test = prompt('enter a digit');
+    if (test > 0 && !isNaN(test)) arr[i] = test;
+else {
+        test = prompt('enter a digit again');
+        if (test > 0 && !isNaN(test)) arr[i] = test;
+        i--;    
+    }
+}
+
+3. Создать массив А размерностью n. Заполнить случайными числами любом диапазоне.
+Найти максимальное число и минимальное число.
+let A = new Array(40);
+
+let min = A[0];
+let max = A[0];
+for ( i = 0; i < A.length; i++ ) {
+    arr[i] = (Math.round( Math.random() * 50 ));
+    if(min > A[i]) min = A[i];
+    if(max < A[i]) max = A[i];
+}
+
+console.log(min, max);
