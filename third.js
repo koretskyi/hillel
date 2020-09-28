01fb1d62808dce416bec974154b413d720d0ff97
koretskyi <koretskyi@gmail.com>
2020-09-28 11:42:20 +0300

    Add files via upload

--- third.js
+++ third.js
@@ -4,9 +4,7 @@ let arr = new Array(40);
 
 for ( i = 0; i < arr.length; i++ ) {
     arr[i] = (Math.round( Math.random() * 50 ));
-    if(i % 2 !== 0 ) {
-        arr[i] = 0;
-    }
+    if (i % 2 !== 0 ) arr[i] = 0;
 }
 
 2. Заполнить массив размерностью n значениями с клавиатуры. Если введенное значение пользователя не число, или отрицательное значение, то запросить повторный ввод данных.
@@ -14,25 +12,27 @@ for ( i = 0; i < arr.length; i++ ) {
 let arr = new Array(5);
 
 for ( i = 0; i < arr.length; i++ ) {
-    let test = prompt('enter a digit');
-    if (test > 0 && !isNaN(test)) arr[i] = test;
-else {
-        test = prompt('enter a digit again');
-        if (test > 0 && !isNaN(test)) arr[i] = test;
-        i--;    
+    arr[i] = +prompt('enter a digit');
+    if (arr[i] < 0 || isNaN(arr[i])){
+    i--;
     }
 }
 
 3. Создать массив А размерностью n. Заполнить случайными числами любом диапазоне.
 Найти максимальное число и минимальное число.
 let A = new Array(40);
+let B = [];
 
-let min = A[0];
-let max = A[0];
+let min = 50;
+let max = 0;
 for ( i = 0; i < A.length; i++ ) {
-    arr[i] = (Math.round( Math.random() * 50 ));
-    if(min > A[i]) min = A[i];
-    if(max < A[i]) max = A[i];
-}
+    A[i] = (Math.round( Math.random() * 50 ));
 
+    if (min > A[i]) min = A[i];
+    if (max < A[i]) max = A[i];
+}
 console.log(min, max);
+
+for ( i = 0; i < A.length; i++ ) {
+    if (A[i] < min && A[i] > max) B[i] = A[i];
+}
