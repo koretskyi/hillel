f5f5dca0ff245fcd35b38805db53bb939c0221cd
koretskyi <koretskyi@gmail.com>
2020-10-19 17:28:27 +0300

    All tasks

--- fifth.js
+++ fifth.js
@@ -1,3 +1,5 @@
+// Вывести в консоль последовательность чисел в обратном порядке, используя рекурсивный вызов функиции
+
 function factorial(n) {
     debugger;
   if (n) {
@@ -6,3 +8,23 @@ function factorial(n) {
         return 1;
     }
 }
+
+
+//  Заполнить массив случайными числами: new Array(new Array(4), new Array(3), new Array(new Array(6), new Array(4)));
+
+let arr = new Array(new Array(4), new Array(3), new Array(new Array(6), new Array(4)));
+
+function func(arr) {
+	for(let i = 0; i < arr.length; i++)
+	{
+		if(arr[i] == undefined) 
+			arr[i] = Math.round( Math.random() * 50 );
+		else {	
+			for(let j = 0; j < arr.length; j++)	
+			func(arr[i]);	
+		}
+	}
+	return arr;
+}
+
+console.log(func(arr));
